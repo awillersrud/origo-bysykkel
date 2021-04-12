1eee6927f7bb4cb8caa0f243a57b73118a827bce
Asbjørn Willersrud <asbjorn.willersrud@embriq.no>
2021-04-12 09:43:33 +0200

    Initial spring boot initializr generated project

---
+++ src/main/java/no/kommune/oslo/kodeoppgave/bysykkel/BysykkelApplication.java
@@ -0,0 +1,13 @@
+package no.kommune.oslo.kodeoppgave.bysykkel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BysykkelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BysykkelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/kommune/oslo/kodeoppgave/bysykkel/BysykkelApplicationTests.java
@@ -0,0 +1,13 @@
+package no.kommune.oslo.kodeoppgave.bysykkel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BysykkelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
